295299b7ea260a4c52ca15dde35fc832576f1228
excelmm <excel.1999@yahoo.com>
2021-12-06 13:36:19 +0800

    d

--- 06/day6-2.py
+++ 06/day6-2.py
@@ -1,2 +1,18 @@
+import copy
 with open("input.txt", "r") as f:
     a = f.read().splitlines()
+
+timer = [int(j) for j in a[0].split(",")]
+
+digit = [0] * 9
+for i in timer:
+    digit[i] += 1
+
+for _ in range(256):
+    j = digit[0]
+    for i in range(8):
+        digit[i] = digit[i + 1]
+    digit[6] += j
+    digit[8] = j
+
+print(sum(digit))
