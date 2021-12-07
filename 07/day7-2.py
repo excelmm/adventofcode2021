fc510c6c0d26e6b5f7d98f0956dbcb111c9c30e1
excelmm <excel.1999@yahoo.com>
2021-12-07 13:10:55 +0800

    d

--- 07/day7-2.py
+++ 07/day7-2.py
@@ -1,2 +1,17 @@
 with open("input.txt", "r") as f:
     a = f.read().splitlines()
+
+positions = [int(i) for i in a[0].split(",")]
+
+leastPos = 0
+leastSum = 1000000000000000
+maxPos = max(positions)
+for i in range(maxPos):
+    sum = 0
+    for j in positions:
+        sum += (abs(i - j) * (abs(i - j) + 1)) / 2
+    if sum < leastSum:
+        leastPos = i
+        leastSum = sum
+
+print(leastSum)
