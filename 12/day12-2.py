21ec33de2748659a621b687bcb870e533c4c4309
excelmm <excel.1999@yahoo.com>
2021-12-12 13:20:32 +0800

    Day 12 Python solutions

---
+++ 12/day12-2.py
@@ -0,0 +1,30 @@
+with open("input.txt", "r") as f:
+    a = f.read().splitlines()
+
+adjList = {}
+paths = set()
+
+def main():
+    for i in a:
+        l = i.split("-")
+        if l[0] not in adjList: adjList[l[0]] = []
+        adjList[l[0]].append(l[1])
+        if l[1] not in adjList: adjList[l[1]] = []
+        adjList[l[1]].append(l[0])
+
+    search('start', '')
+    print(len(paths))
+
+def search(current, path):
+    if current in path and current == 'start': return
+    if '!' in path and current in path and current.islower(): return
+    if current in path and current.islower(): path += '!'
+    path += current
+    if current == 'end':
+        paths.add(path)
+        return
+    if current in adjList:
+        for i in adjList[current]:
+            search(i, path)
+    
+main()
